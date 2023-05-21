6df3a260ed650f7422cc3b90e5b144e8dba92aba
Eceegit <estherraphael126@gmail.com>
2023-05-21 19:08:49 +0100

    updated

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,28 @@
 import React from 'react'
 
+const playlist = [
+  {
+    id: 1,
+    title: "Changed bt Geoffrey",
+    sign: "+"
+  },
+  {
+    id: 1,
+    title: "Changed bt Geoffrey",
+    sign: "+"
+  },
+  {
+    id: 1,
+    title: "Changed bt Geoffrey",
+    sign: "+"
+  },
+  {
+    id: 1,
+    title: "Changed bt Geoffrey",
+    sign: "+"
+  },
+]
+
 function Banner() {
   return (
     <div className='body-container'>
@@ -14,7 +37,11 @@ function Banner() {
 
       <div className='playlist'>
         <div className='playlist-content'>
-
+            {
+              playlist.map(list => (
+                b
+              ))
+            }
         </div>
       </div>
     </div>
